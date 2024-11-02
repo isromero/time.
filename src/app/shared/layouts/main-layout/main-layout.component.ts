b51cab5c770e936e08bd8d9a6db509c2c11b897c
isromero <ismaromeroortega@gmail.com>
2024-11-02 19:57:33 +0100

    Fix sidebar

--- src/app/shared/layouts/main-layout/main-layout.component.ts
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { IconComponent } from '@shared/components/ui/icon.component';
 import { HlmAvatarComponent } from '@spartan-ng/ui-avatar-helm';
 import {
@@ -22,6 +22,8 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [
     RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
     IconComponent,
     HlmAvatarComponent,
     BrnPopoverCloseDirective,
@@ -33,7 +35,6 @@ import { Router } from '@angular/router';
     HlmButtonDirective,
   ],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.css',
 })
 export class MainLayoutComponent {
   themeService: ThemeService = inject(ThemeService);
